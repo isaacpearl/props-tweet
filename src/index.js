e4535471c2113bca7d6312d2b5454f5957fa1a70
isaaacp <pearl.isaac@gmail.com>
2019-02-28 17:12:49 -0500

    css, avatar, message

--- src/index.js
+++ src/index.js
@@ -5,8 +5,39 @@ import './index.css';
 function Tweet() {
 	return (
 		<div className='tweet'>
-			Tweet
+			<Avatar/>
+			<div className="content">
+				<NameWithHandle/>
+				<Message/>
+			</div>
 		</div>
+	);
+}
+
+function Avatar() {
+	return (
+		<img
+			src="https://www.gravatar.com/avatar/nothing"
+			className="avatar"
+			alt="avatar"
+		/>
+	);
+}
+
+function Message() {
+	return (
+		<div className="message">
+			This is less than 140 characters.
+		</div>
+	);
+}
+
+function NameWithHandle() {
+	return (
+		<span className="name-with-handle">
+			<span className="name">Your Name</span>
+			<span className="handle">@yourhandle</span>
+		</span>
 	);
 }
 
